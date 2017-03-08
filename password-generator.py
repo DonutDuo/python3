b552f22df2564ad7a5d71603d26683df4685e3c8
Roy@MacBook <roy2019@gmail.com>
2017-03-08 22:09:27 +0800

    [update] generated password will copy to clipboard

--- password-generator.py
+++ password-generator.py
@@ -1,10 +1,12 @@
 #!/usr/local/bin/python3
 
-import string, random
-
+import string, random, os
+# p1-p4 create 4 digit random string with letters and number
 p1 = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(4)])
 p2 = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(4)])
 p3 = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(4)])
 p4 = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(4)])
-
-print(p1 + "-" + p2 + "-" + p3 + "-" + p4)
+# create passwd in format order
+passwd = p1 + "-" + p2 + "-" + p3 + "-" + p4
+# copy generated password to clipboard
+os.system("echo '%s' | pbcopy" % passwd)
