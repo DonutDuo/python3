4169a25fdd1100c832c298f21ad537380df1c4ce
Roy <roy2019@gmail.com>
2017-03-13 17:14:42 +0800

    increase numberic chances

--- password-generator.py
+++ password-generator.py
@@ -2,10 +2,10 @@
 
 import string, random, os
 # p1-p4 create 4 digit random string with letters and number
-p1 = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(4)])
-p2 = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(4)])
-p3 = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(4)])
-p4 = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(4)])
+p1 = ''.join([random.choice(string.ascii_letters + string.digits + string.digits) for n in range(4)])
+p2 = ''.join([random.choice(string.ascii_letters + string.digits + string.digits) for n in range(4)])
+p3 = ''.join([random.choice(string.ascii_letters + string.digits + string.digits) for n in range(4)])
+p4 = ''.join([random.choice(string.ascii_letters + string.digits + string.digits) for n in range(4)])
 # create passwd in format order
 passwd = p1 + "-" + p2 + "-" + p3 + "-" + p4
 # copy generated password to clipboard
