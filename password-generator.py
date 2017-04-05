3fce776a4c00eca8cdfd68c9868ce2b3e8f0fbca
Roy <roy2019@gmail.com>
2017-04-05 10:56:07 +0800

    [pass] increase numberic

--- password-generator.py
+++ password-generator.py
@@ -4,11 +4,11 @@ import string, random, os
 rno = [4,5,6]
 # p1-p4 create 4 digit random string with letters and number
 rnum = (random.choice(rno))
-p1 = ''.join([random.choice(string.ascii_letters + string.digits + string.digits) for n in range(rnum)])
+p1 = ''.join([random.choice(string.ascii_letters + string.digits + string.digits + string.digits) for n in range(rnum)])
 rnum = (random.choice(rno))
-p2 = ''.join([random.choice(string.ascii_letters + string.digits + string.digits) for n in range(rnum)])
+p2 = ''.join([random.choice(string.ascii_letters + string.digits + string.digits + string.digits) for n in range(rnum)])
 rnum = (random.choice(rno))
-p3 = ''.join([random.choice(string.ascii_letters + string.digits + string.digits) for n in range(rnum)])
+p3 = ''.join([random.choice(string.ascii_letters + string.digits + string.digits + string.digits) for n in range(rnum)])
 # create passwd in format order
 passwd = p1 + "-" + p2 + "-" + p3
 # copy generated password to clipboard
