40e9c874f4b57c49eb1f52f92db9705537c32d80
Roy <roy2019@gmail.com>
2017-04-05 10:37:10 +0800

    [pass] update new format

--- password-generator.py
+++ password-generator.py
@@ -1,12 +1,15 @@
 #!/usr/local/bin/python3
 
 import string, random, os
+rno = [4,5,6]
 # p1-p4 create 4 digit random string with letters and number
-p1 = ''.join([random.choice(string.ascii_letters + string.digits + string.digits) for n in range(4)])
-p2 = ''.join([random.choice(string.ascii_letters + string.digits + string.digits) for n in range(4)])
-p3 = ''.join([random.choice(string.ascii_letters + string.digits + string.digits) for n in range(4)])
-p4 = ''.join([random.choice(string.ascii_letters + string.digits + string.digits) for n in range(4)])
+rnum = (random.choice(rno))
+p1 = ''.join([random.choice(string.ascii_letters + string.digits + string.digits) for n in range(rnum)])
+rnum = (random.choice(rno))
+p2 = ''.join([random.choice(string.ascii_letters + string.digits + string.digits) for n in range(rnum)])
+rnum = (random.choice(rno))
+p3 = ''.join([random.choice(string.ascii_letters + string.digits + string.digits) for n in range(rnum)])
 # create passwd in format order
-passwd = p1 + "-" + p2 + "-" + p3 + "-" + p4
+passwd = p1 + "-" + p2 + "-" + p3
 # copy generated password to clipboard
 os.system("echo '%s' | pbcopy" % passwd)
